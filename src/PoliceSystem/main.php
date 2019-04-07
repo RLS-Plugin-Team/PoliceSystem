7c4599bca87d22be1fe5cd50ce0568a6380c9b82
narapon <43005911+narapons@users.noreply.github.com>
2019-04-07 14:41:42 +0900

    §a【運営】>>>§a{$entity}wo

--- src/PoliceSystem/main.php
+++ src/PoliceSystem/main.php
@@ -4,19 +4,30 @@ namespace PoliceSystem;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\command\CommandExecuter;
-use pocketmine\command\ConsoleCommandSender;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\entity\Effect;
 use pocketmine\entity\EffectInstance;
 use pocketmine\Player;
 use pocketmine\Server;
+use pocketmine\event\player\PlayerEvent;
+use pocketmine\level\Position;
+use pocketmine\level;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\utils\Config;
 
 class main extends PluginBase implements Listener{
 
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->Config = new Config($this->getDataFolder() ."Config.yml", Config::YAML, array(
+                "itemid" => "450",
+                "x" => "1",
+                "t" => "4",
+                "z" => "1",
+                "world" => "world"
+                ));
 		$this->getServer()->getLogger()->info("PoliceSystemを読み込みました");
 	}
 
@@ -37,5 +48,28 @@ class main extends PluginBase implements Listener{
 		    return true;
 		}
 		return true;
-    }
+	}
+	
+	public function onDamage(EntityDamageEvent $event){
+		if($event instanceof EntityDamageByEntityEvent){
+			$damager = $event->getDamager();
+			$entity = $event->getEntity();
+			$itemid = $this->Config->get("itemid");
+			$x = $this->Config->get("x");
+			$y = $this->Config->get("t");
+			$z = $this->Config->get("z");
+			$world = $this->Config->get("world");
+			if($damager instanceof Player && $entity instanceof Player){
+				$item = $damager->getInventory()->getItemInHand();
+				if($item->getId() === $itemid){
+					$event->setCancelled();
+					$pos = new Position($x,$y,$z, $this->getServer()->getLevelByName("{$world}"));
+					$abc = $entity->getName();
+					$def = $damager->getName();
+					$entity->teleport($pos);
+					$damager->sendMessage("
+              }
+          }
+      }
+  }
 }
